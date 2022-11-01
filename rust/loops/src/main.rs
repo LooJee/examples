25b11e23b2b706be0d8769b867b53bbdd3683c63
zer0 <mynameislujie1992@gmail.com>
2022-11-01 11:07:47 +0800

    feat: more loops

--- rust/loops/src/main.rs
+++ rust/loops/src/main.rs
@@ -31,4 +31,36 @@ fn main() {
     };
 
     println!("result : {result}");
+
+    while_loop();
+    for_loop();
+    rev_for_loop();
+}
+
+fn while_loop() {
+    let mut cnt = 3;
+
+    while cnt > 0 {
+        cnt -= 1;
+    }
+
+    println!("while loop end, get cnt : {cnt}");
+}
+
+fn for_loop() {
+    let arr = [1, 2, 3, 4, 5];
+
+    for ele in arr {
+        print!("{ele},")
+    }
+    println!()
+}
+
+fn rev_for_loop() {
+    // (1..4) 左为闭合区间，右为开放区间
+    for ele in (1..4).rev() {
+        print!("{ele},")
+    }
+
+    println!()
 }
