6080699f8a621055bdcf0096fa4835b6dce06619
zer0 <mynameislujie1992@gmail.com>
2023-01-02 16:19:44 +0800

    copy field of struct

--- rust/struct_user/src/main.rs
+++ rust/struct_user/src/main.rs
@@ -2,13 +2,20 @@ fn main() {
     let mut user1 = build_user(String::from("Jonathan"), String::from("jonathan@gmail.com"));
 
     user1.active = false;
+    println!("user1.email : {}", user1.email);
 
     let user2 = User {
         username: String::from("Joshua"),
-        ..user1
+        ..user1 // move user1.email to user2.email
     };
 
-    println!("user2.name : {}", user2.username)
+    println!(
+        "user2.name : {}, user2.email: {}",
+        user2.username, user2.email
+    );
+
+    // will failed here, user1.email has moved
+    // println!("user1.email : {}", user1.email);
 }
 
 fn build_user(username: String, email: String) -> User {
