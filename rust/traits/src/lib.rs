b3bfc45207a82fe74a227b034b44890ea000a96e
zer0 <mynameislujie1992@gmail.com>
2023-02-18 11:20:24 +0800

    feat: advanced trait

--- rust/traits/src/lib.rs
+++ rust/traits/src/lib.rs
@@ -2,6 +2,8 @@ mod post;
 
 #[cfg(test)]
 mod tests {
+    use core::fmt;
+
     use crate::post::Post;
     use crate::{Button, Screen, SelectBox};
 
@@ -42,6 +44,116 @@ mod tests {
 
         post.approve();
         assert_eq!(text, post.content());
+    }
+
+    #[test]
+    fn test_trait_fly() {
+        trait Pilot {
+            fn fly(&self);
+        }
+
+        trait Wizard {
+            fn fly(&self);
+        }
+
+        struct Human;
+
+        impl Pilot for Human {
+            fn fly(&self) {
+                println!("This is your captain speaking.");
+            }
+        }
+
+        impl Wizard for Human {
+            fn fly(&self) {
+                println!("Up!");
+            }
+        }
+
+        impl Human {
+            fn fly(&self) {
+                println!("*waving arms furiously*");
+            }
+        }
+
+        let person = Human;
+
+        person.fly();
+        Wizard::fly(&person);
+        Pilot::fly(&person);
+    }
+
+    #[test]
+    fn test_trait_animal() {
+        trait Animal {
+            fn baby_name() -> String;
+        }
+
+        struct Dog;
+
+        impl Dog {
+            fn baby_name() -> String {
+                String::from("Spot")
+            }
+        }
+
+        impl Animal for Dog {
+            fn baby_name() -> String {
+                String::from("puppy")
+            }
+        }
+
+        println!("A baby dog is called a {}", Dog::baby_name());
+        println!("A baby dog is called a {}", <Dog as Animal>::baby_name());
+    }
+
+    #[test]
+    fn test_super_trait() {
+        trait OutlinePrint: fmt::Display {
+            fn outline_print(&self) {
+                let output = self.to_string();
+                let len = output.len();
+
+                println!("{}", "*".repeat(len + 4));
+                println!("*{}*", " ".repeat(len + 2));
+                println!("* {} *", output);
+                println!("*{}*", " ".repeat(len + 2));
+                println!("{}", "*".repeat(len + 4));
+            }
+        }
+
+        struct Point {
+            x: i32,
+            y: i32,
+        }
+
+        // 因为指定了 OutlinePrint 需要 Display trait，所以，Point 需要实现 Display
+        impl fmt::Display for Point {
+            fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+                write!(f, "({}, {})", self.x, self.y)
+            }
+        }
+
+        impl OutlinePrint for Point {}
+
+        let point = Point { x: 1, y: 2 };
+
+        point.outline_print();
+    }
+
+    #[test]
+    fn test_newtype() {
+        struct Wrapper(Vec<String>);
+
+        impl fmt::Display for Wrapper {
+            fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+                write!(f, "[{}]", self.0.join(","))
+            }
+        }
+
+        let w = Wrapper(vec![String::from("hello"), String::from("world")]);
+
+        println!("{}", w);
     }
 }
 
